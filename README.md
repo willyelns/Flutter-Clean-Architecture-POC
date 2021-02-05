0960656212e07914230a686df11a0e92c969b72a
Will Xavier <willxavier@Wills-MacBook-Pro.local>
2021-02-04 21:41:16 -0300

    feat: first use case

---
+++ README.md
@@ -0,0 +1,10 @@
+<h1 align="center">Clean Architecture POC</h1>
+
+<h5 align="center">A simple project to demonstrate the Clean Architecture in Flutter</h5>
+
+
+<h3>Used technologies</h3>
+
+- MobX (State Managament)
+- Dio (HTTP Client)
+- Get_it (Injection Dependency)
---
+++ lib/features/class/domain/usecases/get_classes.dart
@@ -0,0 +1,14 @@
+import 'package:minhas_aulas/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:minhas_aulas/core/usecases/usecase.dart';
+import 'package:minhas_aulas/features/class/domain/entities/class.dart';
+import 'package:minhas_aulas/features/class/domain/repositories/class_repository.dart';
+
+class GetClasses extends UseCase<List<Class>, NoParams> {
+  GetClasses(this.repository);
+  final ClassRepository repository;
+  @override
+  Future<Either<Failure, List<Class>>> call(NoParams params) {
+    return repository.getClasses();
+  }
+}
